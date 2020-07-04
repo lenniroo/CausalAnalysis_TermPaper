80edf9201af24a5a2a1c2ad2d3265a059415cec8
lenniroo <57395206+lenniroo@users.noreply.github.com>
2020-07-04 22:48:22 +0200

    update scrape.py & the Scrape file is created

--- scrape_tweets.py
+++ scrape_tweets.py
@@ -4,14 +4,14 @@ from twitterscraper import query_tweets
 import datetime as dt
 import pandas as pd
 
-begin_date = dt.date(2015,1,1)
-end_date = dt.date(2015,12,31)
+begin_date = dt.date(2019,11,1)
+end_date = dt.date(2020,5,1)
 
 limit = 10000
 lang = "german"
 
-tweets = query_tweets("Flüchtling", begindate = begin_date, enddate = end_date, limit = limit, lang = lang)
+tweets = query_tweets("flüchtling OR Flüchtling", begindate = begin_date, enddate = end_date, lang = lang)
 
 df = pd.DataFrame(t.__dict__ for t in tweets)
 
-dr.to_csv(r'\Users\Lennart\Documents\GitHub\CausalAnalysis_TermPaper/\Scrape.csv', index = False)
+df.to_csv(r'\Users\Lennart\Documents\GitHub\CausalAnalysis_TermPaper\Scrape.csv', index = False)
